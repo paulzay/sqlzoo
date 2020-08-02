4c3ab7ee172f873c4d54dd5e6fc1df2f987a1376
paulzay <paulotieno2@gmail.com>
2020-08-02 11:14:19 +0300

    ountries bonus exe

--- lib/04a_countries_bonus.rb
+++ lib/04a_countries_bonus.rb
@@ -14,6 +14,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT
+    countries.name
+  FROM
+    countries
+  WHERE
+    countries.gdp > (
+      SELECT
+        MAX(c2.gdp)
+      FROM
+        countries c2
+      WHERE
+        c2.continent = 'Europe'
+    );
   SQL
 end
 
@@ -21,6 +34,21 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+  SELECT
+    c1.continent,
+    c1.name,
+    c1.area
+  FROM
+    countries c1
+  WHERE
+    c1.area = (
+      SELECT
+        MAX(c2.area)
+      FROM
+        countries c2
+      WHERE
+        c1.continent = c2.continent
+    );
   SQL
 end
 
@@ -28,5 +56,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+  SELECT
+    c1.continent,
+    c1.name,
+    c1.area
+  FROM
+    countries c1
+  WHERE
+    c1.area = (
+      SELECT
+        MAX(c2.area)
+      FROM
+        countries c2
+      WHERE
+        c1.continent = c2.continent
+    );
   SQL
 end
